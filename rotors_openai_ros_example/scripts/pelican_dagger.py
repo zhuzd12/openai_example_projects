c452ffed831866294631cdc5534f666b9ab07536
zhuzhengda <zzd16@mails.tsinghua.edu.cn>
2019-01-27 22:24:50 +0800

    fix pelican attitude controller test mode for dagger

--- rotors_openai_ros_example/scripts/pelican_dagger.py
+++ rotors_openai_ros_example/scripts/pelican_dagger.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, default=10000)
     parser.add_argument('--pretrain_epochs', type=int, default=500)
     parser.add_argument('--update_policy_epochs', type=int, default=5)
-    parser.add_argument('--test_frequency', type=int, default=100)
+    parser.add_argument('--test_frequency', type=int, default=50)
     parser.add_argument('--test_epochs', type=int, default=91)
     parser.add_argument('--activation', type=str, default=tf.tanh)
     parser.add_argument('--output_activation', type=str, default=tf.tanh)
@@ -100,17 +100,18 @@ if __name__ == '__main__':
     env = gym.make(args.env)
     A_DIM = env.unwrapped.a_dim
     S_DIM = env.unwrapped.s_dim
-    if args.test_env is not None:
-        test_env = gym.make(args.test_env)
-        assert(A_DIM == test_env.unwrapped.a_dim)
-        assert(S_DIM == test_env.unwrapped.s_dim)
-    else:
-        test_env = env
+    # if args.test_env is not None:
+    #     test_env = gym.make(args.test_env)
+    #     assert(A_DIM == test_env.unwrapped.a_dim)
+    #     assert(S_DIM == test_env.unwrapped.s_dim)
+    # else:
+    #     test_env = env
+    # test_env = env
     
     rospy.loginfo("Gym environment done")
     outdir = '/tmp/openai_ros_experiments/'
     env = gym.wrappers.Monitor(env, outdir, resume=True)
-    test_env = gym.wrappers.Monitor(test_env, outdir, resume=True)
+    # test_env = gym.wrappers.Monitor(test_env, outdir, resume=True)
     call_nlmpc = rospy.ServiceProxy('/pelican/call_nlmpc_controller', AttitudeControllerService)
 
     yaml_path = os.path.abspath('yaml_files/'+args.yaml_file+'.yaml')
@@ -150,7 +151,8 @@ if __name__ == '__main__':
     start_time = time.time()
 
     # start iterating from 'current epoch'.
-    for epoch in range(1, epochs + 1, 1):
+    for epoch in range(epochs):
+        env.unwrapped._set_test_mode(False)
         observation = env.reset()
         cumulated_reward = 0
         # buffer_s, buffer_a, buffer_r = [], [], []
@@ -158,17 +160,13 @@ if __name__ == '__main__':
         for t in range(episode_steps):
             # action = env.action_space.sample()
             # action = ppo.choose_action(np.array(observation))
-            if(epoch <= pre_train_epochs):
-                action = call_mpc(env, call_nlmpc)
+            ref_action = call_mpc(env, call_nlmpc)
+            if(epoch < pre_train_epochs):
+                action = ref_action
             else:
                 action = policy_net.choose_action(np.array(observation), True)
 
             newObservation, reward, done, info = env.step(action)
-
-            if(epoch <= pre_train_epochs):
-                ref_action = action
-            else:
-                ref_action = call_mpc(env, call_nlmpc)
 
             replay_buffer.store(observation, ref_action, reward, newObservation, done)
             observation = newObservation
@@ -184,34 +182,41 @@ if __name__ == '__main__':
                 break
             
         # update policy
-        if epoch % update_policy_epochs == 0 and epoch > 0 :
+        if epoch % update_policy_epochs == 0 and epoch > 0:
             for _ in range(20):
                 batch = replay_buffer.sample_batch(minibatch_size)
                 policy_net.update(batch['obs1'], batch['acts'])
 
         # test policy
-        if epoch % trainning_policy_epochs == 0 and epoch > 0 :
-            if(args.test_env is not None):
-                test_env.unwrapped._reset_initial_index()
+        if epoch % trainning_policy_epochs == 0 and epoch > 0:
+            env.unwrapped._set_test_mode(True)
             for i in range(test_policy_epochs):
-                observation = test_env.reset()
+                observation = env.reset()
                 policy_cumulated_reward = 0
                 p_buffer_s, p_buffer_a = [], []
                 for t in range(episode_steps):
                     action = policy_net.choose_action(np.array(observation), False)
-                    newObservation, reward, done, info = test_env.step(action)
-                    replay_buffer.store(observation, ref_action, reward, newObservation, done)
+                    
+                    # odo = env.unwrapped.get_odom()
+                    # print(odo.pose.pose.position)
+                    # target_pose = env.unwrapped.get_target_pose()
+                    # print("target pose: ", target_pose)
+                    # action = call_mpc(env, call_nlmpc)
+                    # print("observation: ", observation[9:12])
+                    # print("action: ", action)
+                    newObservation, reward, done, info = env.step(action)
+                    # replay_buffer.store(observation, ref_action, reward, newObservation, done)
                     observation = newObservation
                     if (t == episode_steps-1):
                         print ("reached the end")
                         done = True
-                    test_env._flush(force=True)
+                    env._flush(force=True)
                     policy_cumulated_reward += reward
 
                     if done:
                         m, s = divmod(int(time.time() - start_time), 60)
                         h, m = divmod(m, 60)
-                        print("EP " + str(i) + " - {} test number".format(epoch // trainning_policy_epochs) + " - CReward: " + str(round(policy_cumulated_reward, 4)) + "  Time: %d:%02d:%02d" % (h, m, s))
+                        # print("EP " + str(i) + " - {} test number".format(epoch // trainning_policy_epochs) + " - CReward: " + str(round(policy_cumulated_reward, 4)) + "  Time: %d:%02d:%02d" % (h, m, s))
                         policy_net.logger.store(policy_reward=policy_cumulated_reward)
                         policy_net.logger.store(policy_steps=t)
                         break
